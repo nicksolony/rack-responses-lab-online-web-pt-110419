46e700532856155c48e86b4a18d513adf804d444
Nick Solonyy <nicksolony@gmail.com>
2020-02-23 19:16:10 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -4,7 +4,7 @@ class Application
   def call(env)
     resp = Rack::Response.new
     
-    if Time.now.to_i >12
+    if Time.now.to_s.split(" ")[1].split(":")[0].to_i >12
       resp.write "Good Afternoon!"
     else
       resp.write "Good Morning!"
