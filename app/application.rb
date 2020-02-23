08f532d511c4673a648c9f6c68218e97bc5688a0
Nick Solonyy <nicksolony@gmail.com>
2020-02-23 19:07:25 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,8 +1,9 @@
+require 'pry'
 class Application
  
   def call(env)
     resp = Rack::Response.new
- 
+    binding.pry
     num_1 = Kernel.rand(1..20)
     num_2 = Kernel.rand(1..20)
     num_3 = Kernel.rand(1..20)
